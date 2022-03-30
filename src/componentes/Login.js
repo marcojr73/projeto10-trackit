ee323c768d1f244615737dd49d23165eac8928ad
marco junior <marcojuniorengineer@gmail.com>
2022-03-30 07:54:25 -0300

    o usuário já consegue criar uma conta no servidor

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -1,20 +1,19 @@
-import { Link } from "react-router-dom"
-
+import { Link, useNavigate } from "react-router-dom"
+import { useState } from "react/cjs/react.development"
+import axios from "axios"
 import logo from "../assets/images/logo.png"
 import Container from "./layout/start" 
 
 export default function Login(){
-
     
+
     return(
         <Container>
             <img src={logo}/> 
-            <form >
-                <input className="e-mail" placeholder="email" type="email"></input>
-                <input className="pass" placeholder="senha" type="password"></input>
-                {/* <Link to="/habitos"> */}
-                    <button className="sub" type="submit" >Entrar</button>
-                {/* </Link> */}
+            <form onSubmit={logUser} >
+                <input value={email} onChange={(e)=>setEmail(e.target.value)} className="e-mail" placeholder="email" type="email"></input>
+                <input value={password} onChange={(e)=>setPassword(e.target.value)} className="pass" placeholder="senha" type="password"></input>
+                <button className="sub" type="submit" >Entrar</button>
             </form>
             <Link to="/cadastro">
                 <p className="to-register">Não tem uma conta? Cadastre-se!</p>
