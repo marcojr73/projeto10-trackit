ea3a9a3e9c2b7dfa71cb506cf89c856f077bbd6d
marcojr73 <marcojuniorengineer@gmail.com>
2022-09-20 15:58:39 -0300

    deploy

--- src/componentes/teste.js
+++ src/componentes/teste.js
@@ -8,10 +8,13 @@ import logo from "../assets/images/logo.png"
 
 export default function Login(){
     const [load, setLoad] = useState("Entrar")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const url = "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login"
+    const navigate = useNavigate()
     
     function logUser(e){
-        const url = "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login"
-        const objLogin = {email:"email", password:"password"}
+        const objLogin = {email:email, password:password}
         e.preventDefault();
 
         console.log(url, objLogin)
@@ -23,6 +26,7 @@ export default function Login(){
             console.log(data)
             const handle = JSON.stringify(data)
             localStorage.setItem("user", handle)
+            navigate("/hoje")
         })
         promisse.catch(err => {
             setLoad("Entrar")
@@ -34,13 +38,15 @@ export default function Login(){
         <Container>
             <img src={logo} alt="logo"/> 
             <form onSubmit={logUser} >
-                <input  value={"email"}
+                <input  value={email}
+                        onChange={(e)=>setEmail(e.target.value)} 
                         className="e-mail" 
                         placeholder="email" 
                         disabled={load !== "Entrar" ? true : false}
                         type="email">
                 </input>
-                <input  value={"password"} 
+                <input  value={password} 
+                        onChange={(e)=>setPassword(e.target.value)} 
                         className="pass" 
                         placeholder="senha" 
                         disabled={load !== "Entrar" ? true : false}
