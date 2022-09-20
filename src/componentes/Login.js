c6994bf6528a890b3d4e5b415f94d076e7e71d15
marcojr73 <marcojuniorengineer@gmail.com>
2022-09-20 16:10:41 -0300

    deploy

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -1,25 +1,32 @@
 import { Link, useNavigate } from "react-router-dom"
-import { useState } from "react/cjs/react.development"
 import axios from "axios"
 
 import Container from "./layout/start" 
 import Loader from "./layout/loader"
 import logo from "../assets/images/logo.png"
+import { useState } from "react"
 
 export default function Login(){
     const [load, setLoad] = useState("Entrar")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const url = "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login"
+    const navigate = useNavigate()
     
     function logUser(e){
-        const url = "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login"
-        const objLogin = {email: "email", password:"password"}
+        const objLogin = {email:email, password:password}
         e.preventDefault();
 
+        console.log(url, objLogin)
         const promisse = axios.post(url, objLogin)
         setLoad(<Loader/>)
+
         promisse.then(response => {
             const data = response.data;
+            console.log(data)
             const handle = JSON.stringify(data)
             localStorage.setItem("user", handle)
+            navigate("/hoje")
         })
         promisse.catch(err => {
             setLoad("Entrar")
@@ -31,14 +38,18 @@ export default function Login(){
         <Container>
             <img src={logo} alt="logo"/> 
             <form onSubmit={logUser} >
-                <input  value={"email"}
+                <input  value={email}
+                        onChange={(e)=>setEmail(e.target.value)} 
                         className="e-mail" 
                         placeholder="email" 
+                        disabled={load !== "Entrar" ? true : false}
                         type="email">
                 </input>
-                <input  value={"password"} 
+                <input  value={password} 
+                        onChange={(e)=>setPassword(e.target.value)} 
                         className="pass" 
                         placeholder="senha" 
+                        disabled={load !== "Entrar" ? true : false}
                         type="password">
                         
                 </input>
