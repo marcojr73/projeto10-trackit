77df42210eae71b21e40689bc037d5dc1ddafbb4
marcojr73 <marcojuniorengineer@gmail.com>
2022-09-20 09:44:51 -0300

    deploy

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -7,9 +7,54 @@ import Loader from "./layout/loader"
 import logo from "../assets/images/logo.png"
 
 export default function Login(){
+    const [load, setLoad] = useState("Entrar")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const url = "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login"
+    const navigate = useNavigate()
+    
+    function logUser(e){
+        const data = {email, password}
+        e.preventDefault();
+
+        console.log(url, data)
+        const promisse = axios.post(url, data)
+        setLoad(<Loader/>)
+
+        promisse.then(response => {
+            const data = response.data;
+            console.log(data)
+            const handle = JSON.stringify(data)
+            localStorage.setItem("user", handle)
+            navigate("/hoje")
+        })
+        promisse.catch(err => {
+            setLoad("Entrar")
+            alert("usuário não encontrado")
+        })
+    }
+
     return(
         <Container>
             <img src={logo} alt="logo"/> 
+            <form onSubmit={logUser} >
+                <input  value={email}
+                        onChange={(e)=>setEmail(e.target.value)} 
+                        className="e-mail" 
+                        placeholder="email" 
+                        disabled={load !== "Entrar" ? true : false}
+                        type="email">
+                </input>
+                <input  value={password} 
+                        onChange={(e)=>setPassword(e.target.value)} 
+                        className="pass" 
+                        placeholder="senha" 
+                        disabled={load !== "Entrar" ? true : false}
+                        type="password">
+                        
+                </input>
+                <button className="sub" type="submit" >{load}</button>
+            </form>
             <Link to="/cadastro">
                 <p className="to-register">Não tem uma conta? Cadastre-se!</p>
             </Link>
