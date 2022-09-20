8c028890eef5ead49ebb132830eb3a322c31f7ce
marcojr73 <marcojuniorengineer@gmail.com>
2022-09-20 15:55:50 -0300

    deploy

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -7,33 +7,39 @@ import Loader from "./layout/loader"
 import logo from "../assets/images/logo.png"
 
 export default function Login(){
-    const [load, setLoad] = useState("Entrar")
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const url = "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login"
-    const objLogin = {email:email, password:password}
-    const navigate = useNavigate()
+    
+    function logUser(e){
+        const objLogin = {email:"email", password:"password"}
+        e.preventDefault();
+
+        const promisse = axios.post("url", objLogin)
+
+        promisse.then(response => {
+            const data = response.data;
+            const handle = JSON.stringify(data)
+            localStorage.setItem("user", handle)
+        })
+        promisse.catch(err => {
+            alert("usuário não encontrado")
+        })
+    }
 
     return(
         <Container>
             <img src={logo} alt="logo"/> 
-            <form>
-                <input  value={email}
-                        onChange={(e)=>setEmail(e.target.value)} 
+            <form onSubmit={logUser} >
+                <input  value={"email"}
                         className="e-mail" 
                         placeholder="email" 
-                        disabled={load !== "Entrar" ? true : false}
                         type="email">
                 </input>
-                <input  value={password} 
-                        onChange={(e)=>setPassword(e.target.value)} 
+                <input  value={"password"} 
                         className="pass" 
                         placeholder="senha" 
-                        disabled={load !== "Entrar" ? true : false}
                         type="password">
                         
                 </input>
-                <button className="sub" type="submit" >{load}</button>
+                <button className="sub" type="submit" >load</button>
             </form>
             <Link to="/cadastro">
                 <p className="to-register">Não tem uma conta? Cadastre-se!</p>
