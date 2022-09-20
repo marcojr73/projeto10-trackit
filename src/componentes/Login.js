e5c80c1447e677e247f8ad08575966097e76e89d
marcojr73 <marcojuniorengineer@gmail.com>
2022-09-20 09:46:32 -0300

    deploy

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -7,7 +7,6 @@ import Loader from "./layout/loader"
 import logo from "../assets/images/logo.png"
 
 export default function Login(){
-    const [load, setLoad] = useState("Entrar")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const url = "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login"
@@ -19,7 +18,6 @@ export default function Login(){
 
         console.log(url, data)
         const promisse = axios.post(url, data)
-        setLoad(<Loader/>)
 
         promisse.then(response => {
             const data = response.data;
@@ -29,7 +27,6 @@ export default function Login(){
             navigate("/hoje")
         })
         promisse.catch(err => {
-            setLoad("Entrar")
             alert("usuário não encontrado")
         })
     }
@@ -42,18 +39,16 @@ export default function Login(){
                         onChange={(e)=>setEmail(e.target.value)} 
                         className="e-mail" 
                         placeholder="email" 
-                        disabled={load !== "Entrar" ? true : false}
                         type="email">
                 </input>
                 <input  value={password} 
                         onChange={(e)=>setPassword(e.target.value)} 
                         className="pass" 
                         placeholder="senha" 
-                        disabled={load !== "Entrar" ? true : false}
                         type="password">
                         
                 </input>
-                <button className="sub" type="submit" >{load}</button>
+                <button className="sub" type="submit" >load</button>
             </form>
             <Link to="/cadastro">
                 <p className="to-register">Não tem uma conta? Cadastre-se!</p>
